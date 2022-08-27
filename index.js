37dfb5cb9b4f027598bf13890a146d74c268d0d3
Rihab_Atwi <rihab112000@gmail.com>
2022-08-27 22:18:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', function() {
+        console.log('I was clicked!');
+      });
 }
+addingEventListener();
